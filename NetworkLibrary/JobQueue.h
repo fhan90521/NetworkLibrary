eb8292f4fef6cd9fe8fea633e54d5e04873bcdf1
fhan90521 <fhan90521@naver.com>
2024-07-10 17:27:00 +0900

    클라이언트 에서는 j ob q에서 빼는 스레드가 하나일 거고 넣는 스레드와 빼는 스레드가 독립적이게 짜여저 있을거기 때문에 jobq 에 iocpclient 포인터를 넣고 있을 이유가 없다

--- NetworkLibrary/JobQueue.h
+++ NetworkLibrary/JobQueue.h
@@ -6,13 +6,12 @@
 #include "IOCPServer.h"
 #include "IOCPClient.h"
 #include "MyStlContainer.h"
-class JobQueue: public std::enable_shared_from_this<JobQueue>
+class JobQueue : public std::enable_shared_from_this<JobQueue>
 {
 private:
 	friend class IOCPServer;
 	friend class IOCPClient;
 	IOCPServer* _pServer = nullptr;
-	IOCPClient* _pClient = nullptr;
 	Queue<SharedPtr<JobQueue>> _selfPtrQueue;
 	LockFreeQueue<Job*> _jobQueue;
 	char _bProcessing = false;
@@ -22,8 +21,7 @@ protected:
 	ULONG64 _currentTime = 0;
 	bool GetPopAuthority();
 	virtual ~JobQueue();
-	JobQueue(IOCPServer* pServer) :_pServer(pServer) {};
-	JobQueue(IOCPClient* pClient) :_pClient(pClient) {};
+	JobQueue(IOCPServer* pServer = nullptr) :_pServer(pServer) {};
 public:
 	void TryDoSync(CallbackType&& callback)
 	{
@@ -45,20 +43,16 @@ public:
 	void DoAsync(CallbackType&& callback)
 	{
 		_jobQueue.Enqueue(New<Job>(std::move(callback)));
-		if (GetPopAuthority()==true)
+		if (GetPopAuthority() == true)
 		{
 			if (_pServer)
 			{
 				_pServer->PostJob(this);
 			}
-			else if (_pClient)
-			{
-				_pClient->PostJob(this);
-			}
 		}
 	}
 	template<typename T, typename Ret, typename... Args>
-	void DoAsync(Ret(T::* memFunc)(Args...),Args... args)
+	void DoAsync(Ret(T::* memFunc)(Args...), Args... args)
 	{
 		_jobQueue.Enqueue(New<Job>((T*)this, memFunc, std::forward<Args>(args)...));
 		if (GetPopAuthority() == true)
@@ -67,12 +61,26 @@ public:
 			{
 				_pServer->PostJob(this);
 			}
-			else if (_pClient)
-			{
-				_pClient->PostJob(this);
-			}
 		}
 	}
 	int GetProcessedJobCnt();
 	int GetJobQueueLen();
+	// Client¿ë
+	template<typename T, typename Ret, typename... Args>
+	void PushJob(Ret(T::* memFunc)(Args...), Args... args)
+	{
+		_jobQueue.Enqueue(New<Job>((T*)this, memFunc, std::forward<Args>(args)...));
+	}
+	void ProcJobOnClient()
+	{
+		Job* pJob = nullptr;
+		int qSize = _jobQueue.Size();
+		_currentTime = GetTickCount64();
+		for (int i = 0; i < qSize; i++)
+		{
+			_jobQueue.Dequeue(&pJob);
+			pJob->Execute();
+			Delete<Job>(pJob);
+		}
+	}
 };
