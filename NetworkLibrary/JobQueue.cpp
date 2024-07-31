36ca32025f12ec3b86dc6bfbb1070eedc58df525
fhan90521 <fhan90521@naver.com>
2024-07-31 10:21:37 +0900

    주석추가

--- NetworkLibrary/JobQueue.cpp
+++ NetworkLibrary/JobQueue.cpp
@@ -51,6 +51,7 @@ bool JobQueue::GetPopAuthority()
 {
 	while (1)
 	{
+		//Size체크는 필수적인것이고 bSending flag한번체크하는 것은 인터락함수 호출을 조금 줄일 수 있을까 해서 넣은 것v
 		if (_jobQueue.Size() ==0 || _bProcessing == true || InterlockedExchange8(&_bProcessing, true) != false)
 		{
 			return false;
