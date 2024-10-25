d59a5d269ae2c4a27fe2ef75f81a84fa1f5c9aee
fhan90521 <fhan90521@naver.com>
2024-10-25 16:02:29 +0900

    allocate_shared 활용 코드 추가

--- NetworkLibrary/Memory/MakeShared.h
+++ NetworkLibrary/Memory/MakeShared.h
@@ -6,6 +6,10 @@
 template <typename T, typename... Args>
 SharedPtr<T> MakeShared(Args&&... args)
 {
+#ifdef CHECK_ALLOCATINGCNT
 	SharedPtr<T> ptr = { New<T>(std::forward<Args>(args)...), Delete<T> };
+#else
+	SharedPtr<T> ptr = std::allocate_shared<T>(PoolAllocatorForSTL<T>(), std::forward<Args>(args)...);
+#endif
 	return ptr;
 }
