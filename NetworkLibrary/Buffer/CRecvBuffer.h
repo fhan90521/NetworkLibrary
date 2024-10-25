bfe87a9ae1977992369022eac63907f8f3fe17f3
fhan90521 <fhan90521@naver.com>
2024-10-25 20:17:42 +0900

    리팩토링

--- NetworkLibrary/Buffer/CRecvBuffer.h
+++ NetworkLibrary/Buffer/CRecvBuffer.h
@@ -8,18 +8,19 @@ class CRecvBuffer
 {
 private:
 	CRingBuffer* _pBuf;
+	int _remainSize;// 초기값은 네트워크 헤더 Len 링버퍼의 FreeSize랑 구분되어야 함
 	CRecvBuffer(const CRecvBuffer& src) = delete;
 	CRecvBuffer& operator = (CRecvBuffer& src) = delete;
 public:
-	CRecvBuffer(CRingBuffer* pBuf, int size): _pBuf(pBuf){};
+	CRecvBuffer(CRingBuffer* pBuf, int size): _pBuf(pBuf), _remainSize(size) {};
 
 	template <typename T, std::enable_if_t<std::is_arithmetic_v<T> || std::is_same_v<T, wchar_t>,int > uniquifier = 0>
 	CRecvBuffer& operator >> (T& data) 
 	{
-		if (_pBuf->GetFreeSize() < sizeof(T))
+		if (_remainSize < sizeof(T))
 		{
 			//cout << "CRecvBuffer remainSize Error\n";
-			throw(_pBuf->GetFreeSize());
+			throw(_remainSize);
 		}
 		int dequeueSize= _pBuf->Dequeue((char*)&data, sizeof(T));
 		if (dequeueSize != sizeof(T))
@@ -27,16 +28,19 @@ public:
 			Log::LogOnFile(Log::DEBUG_LEVEL, "CRecvBuffer dequeue Error\n");
 			throw(dequeueSize);
 		}
+		_remainSize -= dequeueSize;
 		return *this;
 	}
+
+	
 
 	template <typename T, typename size_t Size, typename = std::enable_if_t<std::is_arithmetic_v<T> || std::is_same_v<T, wchar_t>>>
 	CRecvBuffer& operator >> (Array<T,Size>& arr) 
 	{
-		if (_pBuf->GetFreeSize() < Size * sizeof(T))
+		if (_remainSize < Size * sizeof(T))
 		{
 			Log::LogOnFile(Log::DEBUG_LEVEL, "CRecvBuffer remainSize Error\n");
-			throw(_pBuf->GetFreeSize());
+			throw(_remainSize);
 		}
 		int dequeueSize;
 		dequeueSize = _pBuf->Dequeue((char*)arr.data(), Size * sizeof(T));
@@ -45,17 +49,18 @@ public:
 			Log::LogOnFile(Log::DEBUG_LEVEL, "CRecvBuffer dequeue Error\n");
 			throw(dequeueSize);
 		}
-
+		_remainSize -= dequeueSize;
 		return *this;
 	}
 	
 	int GetData(char* chpDest, int iSize)
 	{
-		if (_pBuf->GetFreeSize() < sizeof(iSize))
+		if (_remainSize < sizeof(iSize))
 		{
 			Log::LogOnFile(Log::SYSTEM_LEVEL, "CRecvBuffer remainSize Error\n");
 		}
 		int dequeueSize = _pBuf->Dequeue(chpDest, iSize);
+		_remainSize -= dequeueSize;
 		return iSize;
 	}
 
@@ -84,9 +89,9 @@ public:
 	}
 
 	template <typename T, std::enable_if_t<std::is_same_v<T, std::string> || std::is_same_v<T, String>
-										||std::is_same_v<T, std::wstring> || std::is_same_v<T, WString>
-	||(std::is_same_v<T, Vector<typename T::value_type>> &&
-	  (std::is_arithmetic_v<typename T::value_type> || std::is_same_v<typename T::value_type, wchar_t>)), int> uniquifier = 0>
+									    ||std::is_same_v<T, std::wstring> || std::is_same_v<T, WString>
+	|| (std::is_same_v<T, Vector<typename T::value_type>>
+		&& (std::is_arithmetic_v<typename T::value_type> || std::is_same_v<typename T::value_type, wchar_t>)),int> uniquifier = 0>
 	CRecvBuffer& operator >> (T& container)
 	{
 		using namespace std;
@@ -94,10 +99,10 @@ public:
 		int dequeueSize;
 
 
-		if (_pBuf->GetFreeSize() < sizeof(containerSize))
+		if (_remainSize < sizeof(containerSize))
 		{
 			Log::LogOnFile(Log::DEBUG_LEVEL, "CRecvBuffer remainSize Error\n");
-			throw(_pBuf->GetFreeSize());
+			throw(_remainSize);
 		}
 
 		dequeueSize = _pBuf->Dequeue((char*)&containerSize, sizeof(containerSize));
@@ -106,12 +111,12 @@ public:
 			Log::LogOnFile(Log::DEBUG_LEVEL, "CRecvBuffer dequeue Error\n");
 			throw(dequeueSize);
 		}
-		
+		_remainSize -= dequeueSize;
 
-		if (_pBuf->GetFreeSize() < containerSize *sizeof(T::value_type))
+		if (_remainSize < containerSize *sizeof(T::value_type))
 		{
 			Log::LogOnFile(Log::DEBUG_LEVEL, "CRecvBuffer remainSize Error\n");
-			throw(_pBuf->GetFreeSize());
+			throw(_remainSize);
 		}
 
 		if (containerSize > container.size())
@@ -126,8 +131,8 @@ public:
 			Log::LogOnFile(Log::DEBUG_LEVEL, "CRecvBuffer dequeue Error\n");
 			throw(dequeueSize);
 		}
+		_remainSize -= dequeueSize;
 
 		return *this;
 	}
-
 };
